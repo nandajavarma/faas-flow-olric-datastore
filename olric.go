4b83c4815adb5d3bf657a5bceeab037558aa363f
Nandaja <nandaja.varma@gmail.com>
2020-11-01 12:06:41 +0100

    Fixing stuff

--- olric.go
+++ olric.go
@@ -43,6 +43,10 @@ func Init() (faasflow.DataStore, error) {
 func (olricstore *OlricDataStore) Configure(flowName string, requestId string) {
 	keyName := fmt.Sprintf("faasflow-%s-%s", flowName, requestId)
 	log.Print("I am inside the configure keyname: %s", keyName)
+	dm := olricstore.olricClient.NewDMap(olricstore.keyName)
+
+	olricstore.dataMap = dm
+	log.Print("Created dmap")
 	olricstore.keyName = keyName
 
 }
@@ -52,9 +56,6 @@ func (olricstore *OlricDataStore) Init() error {
 		return fmt.Errorf("olric client not initialized")
 	}
 
-	dm := olricstore.olricClient.NewDMap(olricstore.keyName)
-
-	olricstore.dataMap = dm
 
 	return nil
 }
